2d94b2fba3cf4a93e08f3bc18dda5a1de5af331a
jooyoung-seo <ijamesseo@gmail.com>
2024-02-13 19:39:01 +1100

    week 1

---
+++ Lecture Notes/LectureNotes.c
@@ -0,0 +1,30 @@
+/*
+
+******
+Week 1
+******
+An operating system sits between our code and the computer, providing essential services
+Terminal (command line driven) allows the sending of simple text commands to our shell
+The shell (bash, zsh) is a program that executes commands, and has its own syntax
+The shell retunrs output which the terminal can display, or can launch other applications
+
+Commands:
+- ls: lists all available directories
+- cd: changes directory
+- mkdir: creates a directory
+- mv [source] [destination]: moves a file from the source to the destination
+- cp [source] [destination]: copies a file from the source to the destination
+- rm [filename]: removes a file
+
+C:
+- a clear language with defined rules, no ambiguity
+- many modern programming languages are based on C (e.g. Python)
+
+Compiling and running code:
+- VSCode: Ctrl+Alt+N is a shortcut to show output in the terminal
+- To actually compile and run the executable:
+    1. cd to the directory where the c file is
+    2. use: "gcc [filename.c] -o filename" to compile
+    3. then: "./filename" to run the program
+
+*/
---
+++ Lecture Notes/Week 1/HelloWorld.c
@@ -0,0 +1,10 @@
+// standard input output
+#include <stdio.h>
+
+// the main function is where the program begins, that is where the execution starts
+int main(void)
+{
+    printf("Hello World\n");
+
+    return 0;
+}
